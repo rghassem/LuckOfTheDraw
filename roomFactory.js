8d2d15ffc9ba0c1bf123195ed6cfbed8d94f316e
Mark Dizon <mark.dizon@makerstudios.com>
2014-03-09 19:10:26 -0700

    Add room generation via string

--- roomFactory.js
+++ roomFactory.js
@@ -9,9 +9,6 @@ var RoomFactory = (function (){
 				room.add(Wall({sprite: '#', room: room}), row, col);
 				console.log("created wall at "+row+" "+col);
 			}
-			else {
-				console.log('gameobject at position'+room.at(row,col));
-			}
 		}
 		// Generate Enemies
 		for(var i = 0; i < 20; ++i) {
@@ -22,15 +19,53 @@ var RoomFactory = (function (){
 					util.getRandomInt(1,constants.roomWidth-1), util.getRandomInt(1,constants.roomHeight-1));
 				console.log("created enemy at "+row+" "+col);
 			}
-			else {
-				console.log('gameobject at position',row,col);
-			}
 		}
 		// Return Room
 		return room;
 	}
 
+	var generateRoom = function(type) {
+		if(type === 'main') {
+			return generateRoomFromString(
+				"............\n" +
+				"............\n" +
+				"..####..##..\n" +
+				"..####..##..\n" +
+				"..##eeee....\n" +
+				"..##eeee....\n" +
+				"..##eeee##..\n" +
+				"..##....##..\n" +
+				"..##....##..\n" +
+				"..##....##..\n" +
+				"............\n" +
+				"............\n"
+			);
+		} else if(type === 'goal') {
+
+		} else {
+			return generateRandomRoom();
+		}
+	};
+
+	var generateRoomFromString = function(str) {
+		var rows = str.split('\n');
+		var room = Room({width: constants.roomWidth, height: constants.roomWidth});
+		for (var i = 0; i < rows.length; ++i) {
+			for (var j = 0; j < rows[i].length; ++j) {
+				var c = rows[i].charAt(j);
+				if(c === '#'){
+					room.add(Wall({sprite:'#',room: room}),i,j);
+				}
+				else if(c === 'e'){
+					room.add(Enemy({sprite:'e',room: room}),i,j);
+				}
+			}
+		}
+		return room;
+	};
+
 	return {
-		generateRandomRoom : generateRandomRoom
+		generateRandomRoom : generateRandomRoom,
+		generateRoom : generateRoom
 	}
 }());
