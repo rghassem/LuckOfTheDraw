ad83f3a93e8aba6b3a6b6bc19cdcf51357c83270
rghassem <rghassem@gmail.com>
2014-03-09 19:42:30 -0700

    Added movement interpolation

--- constants.js
+++ constants.js
@@ -1,6 +1,7 @@
 var constants = {
 	cellSize : 32,
-	roomWidth : 32, 
-	roomHeight : 24,
-	actionQueueDepth : 5
+	roomWidth : 18, 
+	roomHeight : 12,
+	actionQueueDepth : 5,
+	actionDuration : 0.5 //in seconds
 }
